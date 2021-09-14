1f111e469f3a857f1ec8d0c4f9c4b07f42aa2ebe
Chris Battarbee <cbattarbee@palantir.com>
2021-09-14 20:14:24 +0100

    Convert resource_reservation to v1

--- pkg/apis/sparkscheduler/v1beta1/crd_resource_reservation.go
+++ pkg/apis/sparkscheduler/v1beta1/crd_resource_reservation.go
@@ -15,43 +15,49 @@
 package v1beta1
 
 import (
-	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const resourceReservationCRDName = ResourceReservationPlural + "." + GroupName
 
-var resourceReservationDefinition = &apiextensionsv1beta1.CustomResourceDefinition{
+var v1beta1VersionDefinition = apiextensionsv1.CustomResourceDefinitionVersion{
+	Name:    "v1beta1",
+	Served:  true,
+	Storage: true,
+	AdditionalPrinterColumns: []apiextensionsv1.CustomResourceColumnDefinition{{
+		Name:        "driver",
+		Type:        "string",
+		JSONPath:    ".status.driverPod",
+		Description: "Pod name of the driver",
+	}},
+}
+
+var resourceReservationDefinition = &apiextensionsv1.CustomResourceDefinition{
 	ObjectMeta: metav1.ObjectMeta{
 		Name: resourceReservationCRDName,
 	},
-	Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-		Group:   GroupName,
-		Version: "v1beta1",
-		Versions: []apiextensionsv1beta1.CustomResourceDefinitionVersion{
-			{
-				Name:    "v1beta1",
-				Served:  true,
-				Storage: true,
-			},
+	Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+		Group: GroupName,
+		Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+			v1beta1VersionDefinition,
 		},
-		Scope: apiextensionsv1beta1.NamespaceScoped,
-		Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+		Scope: apiextensionsv1.NamespaceScoped,
+		Names: apiextensionsv1.CustomResourceDefinitionNames{
 			Plural:     ResourceReservationPlural,
 			Kind:       "ResourceReservation",
 			ShortNames: []string{"rr"},
 			Categories: []string{"all"},
 		},
-		AdditionalPrinterColumns: []apiextensionsv1beta1.CustomResourceColumnDefinition{{
-			Name:        "driver",
-			Type:        "string",
-			JSONPath:    ".status.driverPod",
-			Description: "Pod name of the driver",
-		}},
 	},
 }
 
 // ResourceReservationCustomResourceDefinition returns the CRD definition for resource reservations
-func ResourceReservationCustomResourceDefinition() *apiextensionsv1beta1.CustomResourceDefinition {
+func ResourceReservationCustomResourceDefinition() *apiextensionsv1.CustomResourceDefinition {
 	return resourceReservationDefinition.DeepCopy()
+}
+
+// ResourceReservationCustomResourceDefinitionVersion returns the CustomResourceDefinitionVersion for the demand resource
+func ResourceReservationCustomResourceDefinitionVersion() apiextensionsv1.CustomResourceDefinitionVersion {
+	return v1beta1VersionDefinition
 }
