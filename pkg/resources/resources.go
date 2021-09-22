a03757411e1885a2a235a770ff5a89940e245b81
Chris Battarbee <cbattarbee@palantir.com>
2021-09-22 19:54:50 +0100

    Add v1Beta2 methods

--- pkg/resources/resources.go
+++ pkg/resources/resources.go
@@ -15,6 +15,7 @@
 package resources
 
 import (
+	"github.com/palantir/k8s-spark-scheduler-lib/pkg/apis/sparkscheduler/v1beta1"
 	"time"
 
 	"github.com/palantir/k8s-spark-scheduler-lib/pkg/apis/sparkscheduler/v1beta2"
@@ -27,7 +28,7 @@ const (
 )
 
 // UsageForNodes tallies resource usages per node from the given list of resource reservations
-func UsageForNodes(resourceReservations []*v1beta2.ResourceReservation) NodeGroupResources {
+func UsageForNodes(resourceReservations []*v1beta1.ResourceReservation) NodeGroupResources {
 	res := NodeGroupResources(map[string]*Resources{})
 	for _, rr := range resourceReservations {
 		for _, reservation := range rr.Spec.Reservations {
@@ -36,6 +37,21 @@ func UsageForNodes(resourceReservations []*v1beta2.ResourceReservation) NodeGrou
 				res[node] = Zero()
 			}
 			res[node].AddFromReservation(&reservation)
+		}
+	}
+	return res
+}
+
+// UsageForNodesV1Beta2 tallies resource usages per node from the given list of resource reservations
+func UsageForNodesV1Beta2(resourceReservations []*v1beta2.ResourceReservation) NodeGroupResources {
+	res := NodeGroupResources(map[string]*Resources{})
+	for _, rr := range resourceReservations {
+		for _, reservation := range rr.Spec.Reservations {
+			node := reservation.Node
+			if res[node] == nil {
+				res[node] = Zero()
+			}
+			res[node].AddFromReservationV1Beta2(&reservation)
 		}
 	}
 	return res
@@ -168,7 +184,13 @@ func Zero() *Resources {
 }
 
 //AddFromReservation modifies the receiver in place.
-func (r *Resources) AddFromReservation(reservation *v1beta2.Reservation) {
+func (r *Resources) AddFromReservation(reservation *v1beta1.Reservation) {
+	r.CPU.Add(reservation.CPU)
+	r.Memory.Add(reservation.Memory)
+}
+
+//AddFromReservationV1Beta2 modifies the receiver in place.
+func (r *Resources) AddFromReservationV1Beta2(reservation *v1beta2.Reservation) {
 	r.CPU.Add(*reservation.Resources.CPU())
 	r.Memory.Add(*reservation.Resources.Memory())
 	r.NvidiaGPU.Add(*reservation.Resources.NvidiaGPU())
