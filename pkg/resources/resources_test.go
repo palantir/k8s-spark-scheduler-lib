9f7989d5adee7d2380d8e6f49b451034200fe215
Chris Battarbee <cbattarbee@palantir.com>
2021-09-22 18:45:24 +0100

    Added tests with zero gpus

--- pkg/resources/resources_test.go
+++ pkg/resources/resources_test.go
@@ -39,10 +39,30 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+func TestAddZeroGpus(t *testing.T) {
+	first := NodeGroupResources(map[string]*Resources{"1": createResources(1, 2, 0), "2": createResources(3, 10, 0)})
+	second := NodeGroupResources(map[string]*Resources{"1": createResources(2, 4, 0), "3": createResources(1, 5, 0)})
+	result := NodeGroupResources(map[string]*Resources{"1": createResources(3, 6, 0), "2": createResources(3, 10, 0), "3": createResources(1, 5, 0)})
+	first.Add(second)
+	if !reflect.DeepEqual(first, result) {
+		t.Fatalf("sum not equal, expected: %+v, got: %+v", result, first)
+	}
+}
+
 func TestSub(t *testing.T) {
 	first := NodeGroupResources(map[string]*Resources{"1": createResources(1, 2, 3), "2": createResources(3, 10, 4)})
 	second := NodeGroupResources(map[string]*Resources{"1": createResources(2, 4, 1), "3": createResources(1, 5, 6)})
 	result := NodeGroupResources(map[string]*Resources{"1": createResources(-1, -2, 2), "2": createResources(3, 10, 4), "3": createResources(-1, -5, -6)})
+	first.Sub(second)
+	if !reflect.DeepEqual(first, result) {
+		t.Fatalf("difference not equal, expected: %+v, got: %+v", result, first)
+	}
+}
+
+func TestSubZeroGpus(t *testing.T) {
+	first := NodeGroupResources(map[string]*Resources{"1": createResources(1, 2, 0), "2": createResources(3, 10, 0)})
+	second := NodeGroupResources(map[string]*Resources{"1": createResources(2, 4, 0), "3": createResources(1, 5, 0)})
+	result := NodeGroupResources(map[string]*Resources{"1": createResources(-1, -2, 0), "2": createResources(3, 10, 0), "3": createResources(-1, -5, 0)})
 	first.Sub(second)
 	if !reflect.DeepEqual(first, result) {
 		t.Fatalf("difference not equal, expected: %+v, got: %+v", result, first)
