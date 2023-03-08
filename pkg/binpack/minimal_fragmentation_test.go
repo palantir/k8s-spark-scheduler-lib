558506f1247979feb883150d7949f77f76e01e68
Alexis Daboville <alexis.daboville@gmail.com>
2023-03-08 16:58:27 +0000

    Move capacity utilities to its own package + make some functions public (#101)
    * Move capacity utilities to its own package + make some functions public
    
    * +license
    
    * lint

--- pkg/binpack/minimal_fragmentation_test.go
+++ pkg/binpack/minimal_fragmentation_test.go
@@ -16,116 +16,11 @@ package binpack
 
 import (
 	"context"
-	"math"
 	"reflect"
 	"testing"
 
 	"github.com/palantir/k8s-spark-scheduler-lib/pkg/resources"
-	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/api/resource"
 )
-
-func TestMin(t *testing.T) {
-	assert.Equal(t, 1, min(1, 2, 3))
-	assert.Equal(t, 1, min(2, 1, 3))
-	assert.Equal(t, 1, min(2, 3, 1))
-}
-
-func TestGetCapacityAgainstSingleDimension(t *testing.T) {
-	assert.Equal(t, math.MaxInt, getCapacityAgainstSingleDimension(
-		*resource.NewQuantity(2, resource.DecimalSI),
-		*resource.NewQuantity(1, resource.DecimalSI),
-		*resource.NewQuantity(0, resource.DecimalSI),
-	))
-	assert.Equal(t, 2, getCapacityAgainstSingleDimension(
-		*resource.NewQuantity(2, resource.DecimalSI),
-		*resource.NewQuantity(0, resource.DecimalSI),
-		*resource.NewQuantity(1, resource.DecimalSI),
-	))
-	assert.Equal(t, 1, getCapacityAgainstSingleDimension(
-		*resource.NewQuantity(3, resource.DecimalSI),
-		*resource.NewQuantity(1, resource.DecimalSI),
-		*resource.NewQuantity(2, resource.DecimalSI),
-	))
-	assert.Equal(t, 0, getCapacityAgainstSingleDimension(
-		*resource.NewQuantity(2, resource.DecimalSI),
-		*resource.NewQuantity(3, resource.DecimalSI),
-		*resource.NewQuantity(1, resource.DecimalSI),
-	))
-}
-
-func TestGetNodeCapacity(t *testing.T) {
-	singleExecutor := &resources.Resources{
-		CPU:       *resource.NewQuantity(1, resource.DecimalSI),
-		Memory:    *resource.NewQuantity(1, resource.DecimalSI),
-		NvidiaGPU: *resource.NewQuantity(1, resource.DecimalSI),
-	}
-
-	tests := []struct {
-		name           string
-		available      *resources.Resources
-		reserved       *resources.Resources
-		singleExecutor *resources.Resources
-		expected       int
-	}{{
-		name:           "no available resources",
-		available:      &resources.Resources{},
-		reserved:       resources.Zero(),
-		singleExecutor: singleExecutor,
-		expected:       0,
-	}, {
-		name:           "available resources fit exactly",
-		available:      singleExecutor,
-		reserved:       resources.Zero(),
-		singleExecutor: singleExecutor,
-		expected:       1,
-	}, {
-		name: "capacity is limited by cpu",
-		available: &resources.Resources{
-			CPU:       *resource.NewQuantity(3, resource.DecimalSI),
-			Memory:    *resource.NewQuantity(4, resource.DecimalSI),
-			NvidiaGPU: *resource.NewQuantity(4, resource.DecimalSI),
-		},
-		reserved:       resources.Zero(),
-		singleExecutor: singleExecutor,
-		expected:       3,
-	}, {
-		name: "capacity is limited by memory",
-		available: &resources.Resources{
-			CPU:       *resource.NewQuantity(4, resource.DecimalSI),
-			Memory:    *resource.NewQuantity(3, resource.DecimalSI),
-			NvidiaGPU: *resource.NewQuantity(4, resource.DecimalSI),
-		},
-		reserved:       resources.Zero(),
-		singleExecutor: singleExecutor,
-		expected:       3,
-	}, {
-		name: "capacity is limited by gpu",
-		available: &resources.Resources{
-			CPU:       *resource.NewQuantity(4, resource.DecimalSI),
-			Memory:    *resource.NewQuantity(4, resource.DecimalSI),
-			NvidiaGPU: *resource.NewQuantity(3, resource.DecimalSI),
-		},
-		reserved:       resources.Zero(),
-		singleExecutor: singleExecutor,
-		expected:       3,
-	}, {
-		name:      "does not fit due to existing reserved resources",
-		available: singleExecutor,
-		reserved: &resources.Resources{
-			CPU: *resource.NewQuantity(1, resource.DecimalSI),
-		},
-		singleExecutor: singleExecutor,
-		expected:       0,
-	},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.expected, getNodeCapacity(test.available, test.reserved, test.singleExecutor))
-		})
-	}
-}
 
 func TestMinimalFragmentation(t *testing.T) {
 	tests := []struct {
---
+++ pkg/capacity/capacity_test.go
@@ -0,0 +1,126 @@
+// Copyright (c) 2019 Palantir Technologies. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package capacity
+
+import (
+	"math"
+	"testing"
+
+	"github.com/palantir/k8s-spark-scheduler-lib/pkg/resources"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, 1, min(1, 2, 3))
+	assert.Equal(t, 1, min(2, 1, 3))
+	assert.Equal(t, 1, min(2, 3, 1))
+}
+
+func TestGetCapacityAgainstSingleDimension(t *testing.T) {
+	assert.Equal(t, math.MaxInt, getCapacityAgainstSingleDimension(
+		*resource.NewQuantity(2, resource.DecimalSI),
+		*resource.NewQuantity(1, resource.DecimalSI),
+		*resource.NewQuantity(0, resource.DecimalSI),
+	))
+	assert.Equal(t, 2, getCapacityAgainstSingleDimension(
+		*resource.NewQuantity(2, resource.DecimalSI),
+		*resource.NewQuantity(0, resource.DecimalSI),
+		*resource.NewQuantity(1, resource.DecimalSI),
+	))
+	assert.Equal(t, 1, getCapacityAgainstSingleDimension(
+		*resource.NewQuantity(3, resource.DecimalSI),
+		*resource.NewQuantity(1, resource.DecimalSI),
+		*resource.NewQuantity(2, resource.DecimalSI),
+	))
+	assert.Equal(t, 0, getCapacityAgainstSingleDimension(
+		*resource.NewQuantity(2, resource.DecimalSI),
+		*resource.NewQuantity(3, resource.DecimalSI),
+		*resource.NewQuantity(1, resource.DecimalSI),
+	))
+}
+
+func TestGetNodeCapacity(t *testing.T) {
+	singleExecutor := &resources.Resources{
+		CPU:       *resource.NewQuantity(1, resource.DecimalSI),
+		Memory:    *resource.NewQuantity(1, resource.DecimalSI),
+		NvidiaGPU: *resource.NewQuantity(1, resource.DecimalSI),
+	}
+
+	tests := []struct {
+		name           string
+		available      *resources.Resources
+		reserved       *resources.Resources
+		singleExecutor *resources.Resources
+		expected       int
+	}{{
+		name:           "no available resources",
+		available:      &resources.Resources{},
+		reserved:       resources.Zero(),
+		singleExecutor: singleExecutor,
+		expected:       0,
+	}, {
+		name:           "available resources fit exactly",
+		available:      singleExecutor,
+		reserved:       resources.Zero(),
+		singleExecutor: singleExecutor,
+		expected:       1,
+	}, {
+		name: "capacity is limited by cpu",
+		available: &resources.Resources{
+			CPU:       *resource.NewQuantity(3, resource.DecimalSI),
+			Memory:    *resource.NewQuantity(4, resource.DecimalSI),
+			NvidiaGPU: *resource.NewQuantity(4, resource.DecimalSI),
+		},
+		reserved:       resources.Zero(),
+		singleExecutor: singleExecutor,
+		expected:       3,
+	}, {
+		name: "capacity is limited by memory",
+		available: &resources.Resources{
+			CPU:       *resource.NewQuantity(4, resource.DecimalSI),
+			Memory:    *resource.NewQuantity(3, resource.DecimalSI),
+			NvidiaGPU: *resource.NewQuantity(4, resource.DecimalSI),
+		},
+		reserved:       resources.Zero(),
+		singleExecutor: singleExecutor,
+		expected:       3,
+	}, {
+		name: "capacity is limited by gpu",
+		available: &resources.Resources{
+			CPU:       *resource.NewQuantity(4, resource.DecimalSI),
+			Memory:    *resource.NewQuantity(4, resource.DecimalSI),
+			NvidiaGPU: *resource.NewQuantity(3, resource.DecimalSI),
+		},
+		reserved:       resources.Zero(),
+		singleExecutor: singleExecutor,
+		expected:       3,
+	}, {
+		name:      "does not fit due to existing reserved resources",
+		available: singleExecutor,
+		reserved: &resources.Resources{
+			CPU: *resource.NewQuantity(1, resource.DecimalSI),
+		},
+		singleExecutor: singleExecutor,
+		expected:       0,
+	},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, GetNodeCapacity(test.available, test.reserved, test.singleExecutor))
+		})
+	}
+}
